fd9f0d54fb7d58ac22d969747b2626bd36fdb092
Samuel Lauro <samuellauro0612@gmail.com>
2024-05-14 11:24:35 -0300

    Update registro.js
    feat registro.js: Adicionando a lógica para adicionar usuário

--- src/view/registro.js
+++ src/view/registro.js
@@ -7,10 +7,70 @@ const Registro = () => {
   const [telefone, setTelefone] = useState('');
   const [usuario, setUsuario] = useState('');
   const [senha, setSenha] = useState('');
+  const [confirmarSenha, setConfirmarSenha] = useState('');
+  const [erroNome, setErroNome] = useState('');
+  const [erroEmail, setErroEmail] = useState('');
+  const [erroTelefone, setErroTelefone] = useState('');
+  const [erroUsuario, setErroUsuario] = useState('');
+  const [erroSenha, setErroSenha] = useState('');
+  const [erroConfirmarSenha, setErroConfirmarSenha] = useState('');
 
   const handleRegistro = () => {
-    // Lógica para registrar o usuário
-    console.log('Registrando novo usuário...');
+    if (!nome) {
+      setErroNome('Por favor, insira seu nome completo.');
+      return;
+    } else {
+      setErroNome('');
+    }
+
+    if (!email) {
+        setErroEmail('Por favor, insira seu e-mail.');
+        return;
+    } else {
+        setErroEmail(''); 
+    }
+
+    if (!telefone) {
+        setErroTelefone('Por favor, insira seu telefone.');
+        return;
+    } else {
+        setErroTelefone('');
+    }
+
+    if (!usuario) {
+        setErroUsuario('Por favor, insira seu nome de usuário.');
+        return;
+    } else {
+        setErroUsuario('');
+    }
+
+    if (!senha) {
+        setErroSenha('Por favor, insira sua senha.');
+        return;
+    } else {
+        setErroSenha('');
+    }
+
+    if (!confirmarSenha) {
+        setErroConfirmarSenha('Por favor, confirme sua senha.');
+        return;
+    } else {
+        setErroConfirmarSenha('');
+    }
+
+    if (senha !== confirmarSenha) {
+        setErroConfirmarSenha('As senhas não coincidem.');
+        return;
+    }
+
+    if (nome && email && telefone && usuario && senha && confirmarSenha) {
+        console.log('Registro realizado com sucesso!');
+    } else { 
+        console.log('Por favor, preencha todos os campos obrigatórios.');
+    }
+
+
+    console.log('Registro realizado com sucesso!');
   };
 
   return (
@@ -22,6 +82,8 @@ const Registro = () => {
         value={nome}
         placeholder="Insira seu nome completo"
       />
+      {erroNome ? <Text style={{ color: 'red' }}>{erroNome}</Text> : null}
+
       <Text style={styles.label}>E-mail</Text>
       <TextInput
         style={styles.input}
@@ -29,6 +91,8 @@ const Registro = () => {
         value={email}
         placeholder="Insira seu e-mail"
       />
+      {erroEmail ? <Text style={{ color: 'red' }}>{erroEmail}</Text> : null}
+
       <Text style={styles.label}>Telefone</Text>
       <TextInput
         style={styles.input}
@@ -36,6 +100,8 @@ const Registro = () => {
         value={telefone}
         placeholder="Insira seu telefone"
       />
+      {erroTelefone ? <Text style={{ color: 'red' }}>{erroTelefone}</Text> : null}
+
       <Text style={styles.label}>Usuário</Text>
       <TextInput
         style={styles.input}
@@ -43,6 +109,8 @@ const Registro = () => {
         value={usuario}
         placeholder="Insira seu nome de usuário"
       />
+      {erroUsuario ? <Text style={{ color: 'red' }}>{erroUsuario}</Text> : null}
+
       <Text style={styles.label}>Senha</Text>
       <TextInput
         style={styles.input}
@@ -51,6 +119,17 @@ const Registro = () => {
         secureTextEntry={true}
         placeholder="Insira sua senha"
       />
+        {erroSenha ? <Text style={{ color: 'red' }}>{erroSenha}</Text> : null}
+
+        <Text style={styles.label}>Confirme sua senha</Text>
+        <TextInput
+            style={styles.input}
+            onChangeText={setConfirmarSenha}
+            value={confirmarSenha}
+            secureTextEntry={true}
+            placeholder="Confirme sua senha"
+        />
+        {erroConfirmarSenha ? <Text style={{ color: 'red' }}>{erroConfirmarSenha}</Text> : null}
       <Button title="Registrar" onPress={handleRegistro} />
     </View>
   );
