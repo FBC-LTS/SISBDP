e0110270e398d3b790263f52f7c06fc390989fa0
Samuel Lauro <samuellauro0612@gmail.com>
2024-05-13 23:42:16 -0300

    Bug dependências
    fix package.json: Resolvido o bug das dependências para o expo go funcionar no celular

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Login from './src/view/login';
+import Registro from './src/view/registro';
 
 const App = () => {
   return (
     <View style={{ flex: 1 }}>
-      <Login />
+      <Login/>
     </View>
   );
 };
