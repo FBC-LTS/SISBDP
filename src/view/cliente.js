5105a98924739fdf7b69a4dc812ad2b5ed866a91
Samuel Lauro <samuellauro0612@gmail.com>
2024-05-31 17:57:03 -0300

    Update cliente.js

--- src/view/cliente.js
+++ src/view/cliente.js
@@ -43,7 +43,7 @@ const Clientes = ({ navigation }) => {
         </ScrollView>
       )}
       <Button title="Adicionar Cliente" onPress={() => navigation.navigate('AdicionarEditarCliente')} />
-      <BottomNavBar navigation={navigation} />
+      
     </View>
   );
 };
