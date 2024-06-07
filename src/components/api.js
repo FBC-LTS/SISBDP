c7422c028b0695fdaf8e65f27ad323f189bff021
Samuel Lauro <samuellauro0612@gmail.com>
2024-06-07 01:41:01 -0300

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -1,14 +1,32 @@
 import axios from 'axios';
 
-const api = {
-  getToken: async (email, password) => {
-  const response = await axios.get(`https://sisbdpapi-production.up.railway.app/token?email=${email}&senha=${password}`).then((response) => {
-    console.log(response.data['token']);
-    return response.data['token'];
-  }).catch((error) => {
-    console.error(error);
-    throw error;
-  });
-}};
+const api = axios.create({
+  baseURL: 'https://sisbdpapi-production.up.railway.app', // Substitua pelo seu endereço da API
+});
+
+api.getToken = async (email, senha) => {
+  try {
+    const response = await api.get('/token', {
+      'email':email,
+      'senha':senha,
+    });
+    return response.data['token']; //Assumindo que a API retorna um objeto com a chave 'token'
+  } catch (error) {
+    console.error('Error fetching token:', error);
+    throw new Error('Failed to fetch token');
+  }
+};
+api.getClientes = async (token) => {
+  try {
+    const response = await api.get('/clientes.json', {
+      token
+    });
+    return response.data; // Assumindo que a API retorna um objeto com a chave 'token'
+  } catch (error) {
+    console.error('Error fetching token:', error);
+    throw new Error('Failed to fetch token');
+  }
+};
+
 
 export default api;
